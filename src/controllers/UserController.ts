1ae8a1439729bdca8f6d3870c0d6aac19d94ad30
Gustavo <gustavograeff1998@gmail.com>
2020-07-30 18:30:20 -0300

    add: encrypt user password before creating  him

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
-import { validationResult, ValidationError } from 'express-validator';
-
 import User from '@schemas/User';
+import { Request, Response, NextFunction } from 'express';
+import { ValidationError, validationResult } from 'express-validator';
+import bcryptjs from 'bcryptjs';
 
 const ERROR_ON_SAVE_DATA = 400;
 const ERROR_ON_VALIDATE_DATA = 422;
@@ -18,29 +18,40 @@ class UserController {
     return res.json(users);
   }
 
-  public async createUser(req: Request, res: Response): Promise<any> {
+  public async createUser(req: Request, res: Response): Promise<Response> {
     const errors = validationResult(req);
 
-    if (errors.isEmpty()) {
-      try {
-        const newUser = await User.create(req.body);
-        return res.json(newUser);
-      } catch (err) {
-        return res.status(ERROR_ON_SAVE_DATA).json(err);
-      }
+    if (!errors.isEmpty()) {
+      const error: IError = {
+        error: 'Validation error',
+        data: errors.array()
+      };
+      return res.status(ERROR_ON_VALIDATE_DATA).json(error);
     }
 
-    const error: IError = {
-      error: 'Validation error',
-      data: errors.array()
-    };
-    return res.status(ERROR_ON_VALIDATE_DATA).json(error);
+    try {
+      req.body.password = await this.encryptUserPassword(req);
+    } catch (err) {
+      return res.status(ERROR_ON_SAVE_DATA).json(err);
+    }
+
+    try {
+      const newUser = await User.create(req.body);
+      return res.json(newUser);
+    } catch (err) {
+      return res.status(ERROR_ON_SAVE_DATA).json(err);
+    }
   }
 
   public async deleteAll(req: Request, res: Response) {
     const user = await User.findOneAndRemove();
 
     return res.json(user);
+  }
+
+  private encryptUserPassword(req: Request): Promise<string> {
+    const { password } = req.body;
+    return bcryptjs.hash(password, 12);
   }
 }
 
