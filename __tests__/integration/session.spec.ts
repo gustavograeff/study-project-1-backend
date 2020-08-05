f80cb7f5713106818bad916b87b444f3b7de9e3f
Gustavo <gustavograeff1998@gmail.com>
2020-08-05 06:42:18 -0300

    add: login tests

--- __tests__/integration/session.spec.ts
+++ __tests__/integration/session.spec.ts
@@ -1,125 +1,54 @@
 import supertest from 'supertest';
 import app from '../../src/app';
-import User from '../../src/schemas/User';
 
-describe('registration', () => {
-  it('Should reset mongo user document', async (done) => {
-    const x = await User.deleteMany({});
-    done();
-  });
-
-  it('should return 422 status, email invalid', async (done) => {
-    const newUser = {
-      firstName: 'Gustavo',
-      lastName: 'Graeff',
+describe('user session', () => {
+  it('should return error when email is not registered', async (done) => {
+    const userCredentials = {
+      email: 'aaaa@aaaa.com',
       password: 'teste'
     };
 
-    const response = await supertest(app).post('/register').send(newUser);
+    const response = await supertest(app).post('/login').send(userCredentials);
+    const responseData = response.body;
 
-    expect(response.status).toBe(422);
+    expect(responseData.message).toEqual('An user with this email could not be found!');
     done();
   });
 
-  it('should return 422 status, firstName invalid', async (done) => {
-    const newUser = {
+  it('should return login error when not passing password', async (done) => {
+    const userCredentials = {
+      email: 'g@g.com'
+    };
+
+    const response = await supertest(app).post('/login').send(userCredentials);
+    const responseData = response.body;
+
+    expect(responseData.requestError).toEqual('Missing password on request body!');
+    done();
+  });
+
+  it('should return invalid password', async (done) => {
+    const userCredentials = {
       email: 'g@g.com',
-      lastName: 'Graeff',
+      password: 'testee'
+    };
+
+    const response = await supertest(app).post('/login').send(userCredentials);
+    const responseData = response.body;
+
+    expect(responseData.message).toEqual('Invalid password!');
+    done();
+  });
+
+  it('should return 200 status if credentials are correct', async (done) => {
+    const userCredentials = {
+      email: 'g@g.com',
       password: 'teste'
     };
 
-    const response = await supertest(app).post('/register').send(newUser);
-
-    expect(response.status).toBe(422);
-    done();
-  });
-
-  it('should return 422 status, lastName invalid', async (done) => {
-    const newUser = {
-      email: 'g@g.com',
-      firstName: 'Gustavo',
-      password: 'teste'
-    };
-
-    const response = await supertest(app).post('/register').send(newUser);
-
-    expect(response.status).toBe(422);
-    done();
-  });
-
-  it('should return 422 status, password invalid', async (done) => {
-    const newUser = {
-      email: 'g@g.com',
-      firstName: 'Gustavo',
-      lastName: 'Graeff'
-    };
-
-    const response = await supertest(app).post('/register').send(newUser);
-
-    expect(response.status).toBe(422);
-    done();
-  });
-
-  it('should return 200 status, user created', async (done) => {
-    const newUser = {
-      email: 'g@g.com',
-      firstName: 'Gustavo',
-      lastName: 'Graeff',
-      password: 'teste'
-    };
-
-    const response = await supertest(app).post('/register').send(newUser);
+    const response = await supertest(app).post('/login').send(userCredentials);
 
     expect(response.status).toBe(200);
-    done();
-  });
-
-  it('should trim body and encrypt user password on register', async (done) => {
-    const newUser = {
-      email: 'g@g.co',
-      firstName: 'Gustavo',
-      lastName: 'Graeff',
-      password: 'teste!'
-    };
-
-    const response = await supertest(app).post('/register').send(newUser);
-    const responseData = JSON.parse(response.text);
-
-    expect(responseData.email).toEqual(newUser.email.trim());
-    expect(responseData.firstName).toEqual(newUser.firstName.trim());
-    expect(responseData.lastName).toEqual(newUser.lastName.trim());
-    expect(responseData.password).not.toEqual(newUser.password);
-    done();
-  });
-
-  it('should apply .trim() to fix every body data spaces', async (done) => {
-    const newUser = {
-      email: ' g@g.co ',
-      firstName: ' Gustavo ',
-      lastName: ' Graeff ',
-      password: ' teste! '
-    };
-
-    const response = await supertest(app).post('/register').send(newUser);
-    const responseData = JSON.parse(response.text);
-
-    expect(responseData.email).not.toEqual(newUser.email.trim());
-    expect(responseData.firstName).not.toEqual(newUser.firstName.trim());
-    expect(responseData.lastName).not.toEqual(newUser.lastName.trim());
-    done();
-  });
-
-  it('should return 422 status, email already registered', async (done) => {
-    const newUser = {
-      email: 'g@g.com',
-      firstName: 'Gustavo',
-      lastName: 'Graeff',
-      password: 'teste'
-    };
-
-    const response = await supertest(app).post('/register').send(newUser);
-
-    expect(response.status).toBe(422);
     done();
   });
 });
