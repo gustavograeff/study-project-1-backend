bbc2bad2c10912bd4af685af6ec0fb466c99b55d
Gustavo <gustavograeff1998@gmail.com>
2020-08-05 06:14:10 -0300

    add: registration new tests

--- __tests__/integration/session.spec.ts
+++ __tests__/integration/session.spec.ts
@@ -1,7 +1,13 @@
 import supertest from 'supertest';
 import app from '../../src/app';
+import User from '../../src/schemas/User';
 
 describe('registration', () => {
+  it('Should reset mongo user document', async (done) => {
+    const x = await User.deleteMany({});
+    done();
+  });
+
   it('should return 422 status, email invalid', async (done) => {
     const newUser = {
       firstName: 'Gustavo',
@@ -46,6 +52,69 @@ describe('registration', () => {
       email: 'g@g.com',
       firstName: 'Gustavo',
       lastName: 'Graeff'
+    };
+
+    const response = await supertest(app).post('/register').send(newUser);
+
+    expect(response.status).toBe(422);
+    done();
+  });
+
+  it('should return 200 status, user created', async (done) => {
+    const newUser = {
+      email: 'g@g.com',
+      firstName: 'Gustavo',
+      lastName: 'Graeff',
+      password: 'teste'
+    };
+
+    const response = await supertest(app).post('/register').send(newUser);
+
+    expect(response.status).toBe(200);
+    done();
+  });
+
+  it('should trim body and encrypt user password on register', async (done) => {
+    const newUser = {
+      email: 'g@g.co',
+      firstName: 'Gustavo',
+      lastName: 'Graeff',
+      password: 'teste!'
+    };
+
+    const response = await supertest(app).post('/register').send(newUser);
+    const responseData = JSON.parse(response.text);
+
+    expect(responseData.email).toEqual(newUser.email.trim());
+    expect(responseData.firstName).toEqual(newUser.firstName.trim());
+    expect(responseData.lastName).toEqual(newUser.lastName.trim());
+    expect(responseData.password).not.toEqual(newUser.password);
+    done();
+  });
+
+  it('should apply .trim() to fix every body data spaces', async (done) => {
+    const newUser = {
+      email: ' g@g.co ',
+      firstName: ' Gustavo ',
+      lastName: ' Graeff ',
+      password: ' teste! '
+    };
+
+    const response = await supertest(app).post('/register').send(newUser);
+    const responseData = JSON.parse(response.text);
+
+    expect(responseData.email).not.toEqual(newUser.email.trim());
+    expect(responseData.firstName).not.toEqual(newUser.firstName.trim());
+    expect(responseData.lastName).not.toEqual(newUser.lastName.trim());
+    done();
+  });
+
+  it('should return 422 status, email already registered', async (done) => {
+    const newUser = {
+      email: 'g@g.com',
+      firstName: 'Gustavo',
+      lastName: 'Graeff',
+      password: 'teste'
     };
 
     const response = await supertest(app).post('/register').send(newUser);
