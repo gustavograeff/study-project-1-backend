aea5c4b5a81ff46bcc7112401b676e26db44c4d5
Gustavo <gustavograeff1998@gmail.com>
2020-08-05 05:47:54 -0300

    add: registration fail tests

--- __tests__/integration/session.spec.ts
+++ __tests__/integration/session.spec.ts
@@ -2,7 +2,46 @@ import supertest from 'supertest';
 import app from '../../src/app';
 
 describe('registration', () => {
-  it('should return 422 status code when em-mail already exists', async (done) => {
+  it('should return 422 status, email invalid', async (done) => {
+    const newUser = {
+      firstName: 'Gustavo',
+      lastName: 'Graeff',
+      password: 'teste'
+    };
+
+    const response = await supertest(app).post('/register').send(newUser);
+
+    expect(response.status).toBe(422);
+    done();
+  });
+
+  it('should return 422 status, firstName invalid', async (done) => {
+    const newUser = {
+      email: 'g@g.com',
+      lastName: 'Graeff',
+      password: 'teste'
+    };
+
+    const response = await supertest(app).post('/register').send(newUser);
+
+    expect(response.status).toBe(422);
+    done();
+  });
+
+  it('should return 422 status, lastName invalid', async (done) => {
+    const newUser = {
+      email: 'g@g.com',
+      firstName: 'Gustavo',
+      password: 'teste'
+    };
+
+    const response = await supertest(app).post('/register').send(newUser);
+
+    expect(response.status).toBe(422);
+    done();
+  });
+
+  it('should return 422 status, password invalid', async (done) => {
     const newUser = {
       email: 'g@g.com',
       firstName: 'Gustavo',
