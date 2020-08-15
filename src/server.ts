ca4b5d406681229a8d1f3f4213709b9de39b5785
Gustavo <gustavograeff1998@gmail.com>
2020-08-15 20:59:42 -0300

    add: commented code to check later response header access control

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import app from './app';
 
 const PORT = process.env.PORT || 8080;
 
-app.use((req, res, next) => {
+// check later
+/* app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-});
+}); */
 
 app.listen(PORT);
