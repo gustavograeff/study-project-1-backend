ce3b5e505f3f5f976cce2876e5a1b4e39ed54845
gustavograeff <gustavograeff1998@gmail.com>
2020-11-09 07:34:05 -0300

    add: env_password into dev script and mongo string reference

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,9 @@ dotenv.config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 });
 
-const MONGO_DB_URL = `mongodb+srv://${process.env.ENV_USER}:0Et9ab0y2WlC438l@cluster0-lqxgu.mongodb.net/${process.env.ENV_DB}`;
+console.log(process.env.ENV_PASSWORD);
+
+const MONGO_DB_URL = `mongodb+srv://${process.env.ENV_USER}:${process.env.ENV_PASSWORD}@cluster0-lqxgu.mongodb.net/${process.env.ENV_DB}`;
 class App {
   public express: express.Application;
 
